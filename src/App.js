dca9d098448bff4ff39a68dbafd320ed51276f1b
Almujtaba Abdalla <almujtaba007@gmail.com>
2022-05-26 14:45:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home'
+// import logo from './Assets/final_logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+    </>
+    // <nav className="relative container mx-auto p-6">
+    //   {/* Flex Container */}
+    //   <div className="flex items-center justify-between">
+    //     {/* Logo */}
+    //     <div className="pt-2 ">
+    //       <img src={logo} alt="React Logo" className='max-h-10'/>
+    //     </div>
+    //     {/* Menu items */}
+    //     <div className="hidden md:flex space-x-6">
+    //       <a href="#" className="hover:text-red-500">Home</a>
+    //       <a href="#" className="hover:text-red-500">About</a>
+    //       <a href="#" className="hover:text-red-500">Service</a>
+    //       <a href="#" className="hover:text-red-500">Portfolio</a>
+    //       <a href="#" className="hover:text-red-500">Blog</a>
+    //       <a href="#" className="hover:text-red-500">Contact</a>
+    //     </div>
+    //     {/* Button */}
+    //     <a href="#" className="hidden md:block p-3 px-6 pt-2 text-white bg-red-500 rounded-full baseline hover:bg-red-400" > Hire Me </a>
+    //   {/* Hamburger Menu */}
+
+    //   </div>
+    // </nav>
   );
 }
 
