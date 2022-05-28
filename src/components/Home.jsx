06fa6e79ef3792a864342672da391d5fe69cb8be
Almujtaba Abdalla <almujtaba007@gmail.com>
2022-05-28 22:11:30 +0200

    motivation for leena

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,14 @@ import Typed from "react-typed";
 export default function Home() {
   return (
     <div className="container p-8 mt-1 md:p-24 md:mt-32 space-y-6 ">
-      <h1 className="text-gray-300 md:text-6xl text-4xl ">Hi, I'm Almujtaba</h1>
+      {/* <h1 className="text-gray-300 md:text-6xl text-4xl ">Hi, I'm Almujtaba</h1> */}
       <Typed className="flex md:text-6xl text-4xl text-red-500 " 
-            strings={["Python Developer", "React Developer"]} 
-            typeSpeed={60}
-            loop
+            strings={["Hey Leena 👋", "I know that..", "you having a hard times", "But remeber It's temporary", "and It will be better soon..", "Trust yourself", "you will be just fine!", "You got this!", "May allah help you", "Good luck Leena 😊💕"]} 
+            typeSpeed={70}
+            // loop
              />
-      <h1 className="text-gray-300 md:text-6xl text-4xl ">Based in Sudan.</h1>
+      
+      {/* <h1 className="text-gray-300 md:text-6xl text-4xl ">Based in Sudan.</h1> */}
     </div>
   );
 }
