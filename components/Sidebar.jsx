e8c45a6805c9ec2987443eb7bd6e070582a4a40c
Almujtaba Abdalla <almujtaba007@gmail.com>
2023-10-12 17:30:20 +0200

    update projects page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,46 +1,20 @@
-import React from "react";
-import {BsArrowLeftShort} from "react-icons/bs";
-import { useState } from "react";
-// import { Disclosure, Transition } from "@headlessui/react";
+import React, { useState } from "react";
+import { BsArrowLeftShort } from "react-icons/bs";
 import { MdHome, MdInfo, MdContactPage, MdFolder } from "react-icons/md";
 import { FaHandHoldingHeart } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
-import {Link} from "react-scroll"
+import { Link } from "react-scroll";
+
+// Define navigation outside the component to avoid re-rendering
+const navigation = [
+  { name: "Home", to: "home", icon: MdHome },
+  { name: "Service", to: "service", icon: FaHandHoldingHeart },
+  { name: "Projects", to: "projects", icon: MdFolder },
+  { name: "Contact", to: "contact", icon: MdContactPage },
+  { name: "About", to: "about", icon: MdInfo },
+];
 
 function Sidebar() {
   const [open, setOpen] = useState(false);
-  const navigation = [
-    {
-      name: "Home",
-      to: "home",
-      current: true,
-      icon: MdHome,
-    },
-    {
-      name: "Service",
-      to: "service",
-      current: false,
-      icon: FaHandHoldingHeart,
-    },
-    {
-      name: "Projects",
-      to: "projects",
-      current: false,
-      icon: MdFolder,
-    },
-    {
-      name: "Contact",
-      to: "contact",
-      current: false,
-      icon: MdContactPage,
-    },
-    {
-      name: "About",
-      to: "about",
-      current: false,
-      icon: MdInfo,
-    },
-  ];
 
   return (
     <div className="sm:hidden flex items-center top-0 right-0 h-full justify-center fixed">
@@ -48,32 +22,23 @@ function Sidebar() {
         <BsArrowLeftShort className={`bg-blue-900 text-3xl rounded-full absolute -left-4 top-9 cursor-pointer ${open && "rotate-180"}`} onClick={() => setOpen(!open)} />
         <div className={`flex justify-center`}>
           <ul className="space-y-2">
-                    {navigation.map((item) => (
-                      <li>
-                        <Link
-                          key={item.name}
-                          activeClass="underline underline-offset-8 decoration-red-500 text-red-500"
-                          to={item.to}
-                          smooth={true}
-                          spy={true}
-                          duration={300}
-                          offset={-90}
-                          // className={({ isActive }) =>
-                          //   isActive
-                          //     ? "underline underline-offset-8 decoration-red-500 text-red-500"
-                          //     : ""
-                          // }
-                        >
-                          <button
-                          className={`flex items-center p-2 text-base font-normal`}
-                          >
-                            
-                            <span className={`${!open && "space-y-2"}`}>{<item.icon size={open ? 20 : 24}/>}</span>
-                            <span className={`ml-3 duration-300 ${!open && "scale-0 absolute"}`}>{item.name}</span>
-                          </button>
-                        </Link>
-                      </li>
-                    ))}
+            {navigation.map((item) => (
+              <li key={item.name}>
+                <Link
+                  activeClass="underline underline-offset-8 decoration-red-500 text-red-500"
+                  to={item.to}
+                  smooth={true}
+                  spy={true}
+                  duration={300}
+                  offset={-90}
+                >
+                  <button className={`flex items-center p-2 text-base font-normal`}>
+                    <span className={`${!open && "space-y-2"}`}>{<item.icon size={open ? 20 : 24}/>}</span>
+                    <span className={`ml-3 duration-300 ${!open && "scale-0 absolute"}`}>{item.name}</span>
+                  </button>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,57 +1,62 @@
 import React from "react";
 import Image from "next/image";
-import service from "public/service.webp"
-import service2 from "public/service2.webp"
-import service3 from "public/service3.webp"
+import service from "public/service.webp";
+import service2 from "public/service2.webp";
+import service3 from "public/service3.webp";
 
 const projects = [
   {
     name: "Portfolio Website",
-    description: "This is my personal portfolio website, built with React, Node.js, and MySQL.",
+    description:
+      "This is my personal portfolio website, built with React, Node.js, and MySQL.",
     image: service,
-    link: "https://example.com",
+    link: "https://images.app.goo.gl/kr5WKjGEoEHj19re6",
   },
   {
     name: "E-Commerce Website",
-    description: "This is an e-commerce website that I built for a client, using React, Node.js, and MySQL.",
+    description:
+      "This is an e-commerce website that I built for a client, using React, Node.js, and MySQL.",
     image: service2,
-    link: "https://example.com/shop",
+    link: "https://images.app.goo.gl/kr5WKjGEoEHj19re6",
   },
   {
     name: "Mobile App",
-    description: "This is a mobile app that I built for a client, using React Native.",
+    description:
+      "This is a mobile app that I built for a client, using React Native.",
     image: service3,
-    link: "https://example.com/mobile-app",
+    link: "https://images.app.goo.gl/kr5WKjGEoEHj19re6",
   },
   {
-    name: "Mobile App",
-    description: "This is a mobile app that I built for a client, using React Native.",
+    name: "Phone App",
+    description:
+      "This is a mobile app that I built for a client, using React Native.",
     image: service3,
-    link: "https://example.com/mobile-app",
+    link: "https://images.app.goo.gl/kr5WKjGEoEHj19re6",
   },
 ];
 
 function Projects() {
   return (
-    <section className="container mx-auto text-gray-100" id="projects">
-      <div className="text-center before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-cyan-400">
+    <section
+      className="text-center p-8 mx-auto text-gray-100"
+      id="projects"
+    >
+      <div className="text-center before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-cyan-400 mb-8">
         <h3 className="text-3xl font-semibold text-cyan-400">Projects</h3>
         <span className="text-sm font-bold tracking-wider uppercase dark:text-gray-400">
           What I've Built
         </span>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-12 sm:p-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
-          <div key={project.name} className="flex flex-col justify-center items-center">
-            <div className="shadow-xl rounded-md overflow-hidden">
-              <Image src={project.image} alt={project.name} />
-              <div className="p-8">
-                <h3 className="text-2xl font-semibold">{project.name}</h3>
-                <p className="mb-6">{project.description}</p>
-                <a href={project.link} className="block font-medium text-primary">
-                  Learn More
-                </a>
-              </div>
+          <div
+            key={project.id}
+            className="rounded overflow-hidden shadow-lg transform transition duration-500 hover:scale-105 cursor-pointer"
+          >
+            <Image className="w-full" src={project.image} alt={project.name} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{project.name}</div>
+              <p className="text-gray-700 text-base">{project.description}</p>
             </div>
           </div>
         ))}
