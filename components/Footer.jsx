88d0097db175ab549d018168351417b7b48aedaf
Almujtaba Abdalla <almujtaba007@gmail.com>
2023-11-11 19:06:16 +0200

    update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,30 +1,8 @@
 const Footer = () => {
     return (
-      <footer className="mx-auto my-4 w-full max-w-7xl px-8 py-4 text-center text-[0.8rem] text-white">
+      <footer className="mx-auto w-full px-8 py-2 my-2 text-center text-[0.8rem] text-white">
         <span>
-          &copy; {new Date().getFullYear()} Almujtaba Abdalla. Find me on{" "}
-          <a
-            href="https://www.linkedin.com/in/rajesh-babu-56270046/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin
-          </a>{" "}
-          or{" "}
-          <a
-            href="https://twitter.com/razzmattazzz"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-          </a>
-          .
-          <br />
-          Powered by{" "}
-          <a href="https://remix.run/" target="_blank" rel="noopener noreferrer">
-            Almujtaba
-          </a>
-          .
+          &copy; {new Date().getFullYear()} Powered by Almujtaba.
         </span>
       </footer>
     );
