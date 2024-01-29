e5209bc8c77a99ffedc95bfb65b97ba18ca1b999
Almujtaba Abdalla <almujtaba007@gmail.com>
2024-01-29 12:09:13 +0200

    change form button color

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -94,7 +94,7 @@ function Contact() {
             <div>
               <button
                 type="submit"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               >
                 Send Message
               </button>
