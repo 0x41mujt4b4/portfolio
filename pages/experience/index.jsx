8d553fa329a31e800b63f389a756f95ae1de9cfb
Almujtaba Abdalla <almujtaba007@gmail.com>
2024-01-29 12:23:02 +0200

    add title component to experience page

--- pages/experience/index.jsx
+++ pages/experience/index.jsx
@@ -1,3 +1,5 @@
+import Title from "@/components/Title";
+
 const experiences = [
   {
     name: "Software Engineer @ RaasMalArabi",
@@ -25,12 +27,7 @@ function Experience() {
       <div className="container max-w-5xl px-4 py-12 mx-auto">
         <div className="grid gap-4 mx-4 sm:grid-cols-12">
           <div className="col-span-12 sm:col-span-3">
-            <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-cyan-400">
-              <h3 className="text-3xl font-semibold text-cyan-400">Experiences</h3>
-              <span className="text-sm font-bold tracking-wider uppercase dark:text-gray-400">
-                Where I've Worked
-              </span>
-            </div>
+            <Title title="Experiences" subtitle="Where I've Worked"/>
           </div>
           <div className="relative col-span-12 px-4 space-y-6 sm:col-span-9">
             <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:dark:bg-gray-700">
